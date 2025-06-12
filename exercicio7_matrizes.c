2d16283affe9319390fc565843ee2e7d3e019547
Adolfo Neto <adolfo.usp@gmail.com>
2025-06-12 15:27:32 -0300

    Usando funções para matrizes com passagem de parâmetros
    Não use variáveis globais.

--- exercicio7_matrizes.c
+++ exercicio7_matrizes.c
@@ -4,75 +4,96 @@
 
 #define N 10
 
-int matCriada = 0;
-int mat1[N][N], mat2[N][N], resultado[N][N];
 
-void criaMatrizes() {
+
+void criaMatrizes(int mat1[N][N],int mat2[N][N])
+{
     srand(time(NULL));
     for (int i=0; i<N; i++)
-        for (int j=0; j<N; j++) {
+        for (int j=0; j<N; j++)
+        {
             mat1[i][j] = rand() % 100;
             mat2[i][j] = rand() % 100;
         }
-    matCriada = 1;
 }
 
-void imprimeMatriz(int m[N][N]) {
-    for (int i=0; i<N; i++) {
+void imprimeMatriz(int m[N][N])
+{
+    for (int i=0; i<N; i++)
+    {
         for (int j=0; j<N; j++)
             printf("%d ", m[i][j]);
         printf("\n");
     }
 }
 
-void somaMatrizes() {
+void somaMatrizes(int mat1[N][N],int mat2[N][N],int resultado[N][N])
+{
     for (int i=0; i<N; i++)
         for (int j=0; j<N; j++)
             resultado[i][j] = mat1[i][j] + mat2[i][j];
 }
 
-void subtraiMatrizes() {
+void subtraiMatrizes(int mat1[N][N],int mat2[N][N], int resultado[N][N])
+{
     for (int i=0; i<N; i++)
         for (int j=0; j<N; j++)
             resultado[i][j] = mat1[i][j] - mat2[i][j];
 }
 
-void multiplicaConstante(int c) {
+void multiplicaConstante(int mat1[N][N],int resultado[N][N], int c)
+{
     for (int i=0; i<N; i++)
         for (int j=0; j<N; j++)
             resultado[i][j] = mat1[i][j] * c;
 }
 
-int main() {
+int main()
+{
+
+    int matCriada = 0;
+    int mat1[N][N], mat2[N][N], resultado[N][N];
+
     int op, c;
-    do {
+    do
+    {
         printf("Menu:\n0 - Criar matrizes\n1 - Imprimir matrizes\n2 - Somar matrizes\n3 - Subtrair matrizes\n4 - Multiplicar primeira matriz por constante\n5 - Sair\n");
         scanf("%d", &op);
-        if (op == 0) criaMatrizes();
+        if (op == 0)
+        {
+            criaMatrizes(mat1, mat2);
+            matCriada = 1;
+        }
+
         else if ((op>=1 && op<=4) && !matCriada) printf("Matrizes nao criadas.\n");
-        else if (op == 1) {
+        else if (op == 1)
+        {
             printf("Matriz 1:\n");
             imprimeMatriz(mat1);
             printf("Matriz 2:\n");
             imprimeMatriz(mat2);
         }
-        else if (op == 2) {
-            somaMatrizes();
+        else if (op == 2)
+        {
+            somaMatrizes(mat1, mat2, resultado);
             printf("Resultado da soma:\n");
             imprimeMatriz(resultado);
         }
-        else if (op == 3) {
-            subtraiMatrizes();
+        else if (op == 3)
+        {
+            subtraiMatrizes(mat1, mat2, resultado);
             printf("Resultado da subtracao:\n");
             imprimeMatriz(resultado);
         }
-        else if (op == 4) {
+        else if (op == 4)
+        {
             printf("Constante: ");
             scanf("%d", &c);
-            multiplicaConstante(c);
+            multiplicaConstante(mat1, resultado, c);
             printf("Resultado da multiplicacao:\n");
             imprimeMatriz(resultado);
         }
-    } while(op != 5);
+    }
+    while(op != 5);
     return 0;
 }
